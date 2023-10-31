f2babc752c5a787e784c6a51ee19d9fef548521d
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-10-31 17:04:49 -0400

    add parameters

--- test/test_grid_tensor.py
+++ test/test_grid_tensor.py
@@ -11,7 +11,9 @@ def test_raw_creation():
 @pytest.mark.parametrize("max_value", [0.2, 0.9])
 @pytest.mark.parametrize("log_scale", [True, False])
 @pytest.mark.parametrize("shape", [1, 64, (2, 2), (64, 1, 1)])
-def test_from_range():
+def test_from_range(
+    min_value: float, max_value: float, log_scale: bool, shape: int | tuple
+):
     ...
 
 
--- test/test_normed_tensor.py
+++ test/test_normed_tensor.py
@@ -5,13 +5,13 @@ import psst
 
 @pytest.mark.parametrize("is_normalized", [True, False])
 @pytest.mark.parametrize("log_scale", [True, False])
-def test_normalize():
+def test_normalize(is_normalized: bool, log_scale: bool):
     ...
 
 
 @pytest.mark.parametrize("is_normalized", [True, False])
 @pytest.mark.parametrize("log_scale", [True, False])
-def test_unnormalize():
+def test_unnormalize(is_normalized: bool, log_scale: bool):
     ...
 
 
@@ -20,7 +20,7 @@ def test_repeated_norm_unnorm():
 
 
 @pytest.mark.parametrize("log_scale", [True, False])
-def test_generate():
+def test_generate(log_scale: bool):
     ...
 
 
@@ -30,11 +30,11 @@ def test_create_raw():
 
 @pytest.mark.parametrize("is_normalized", [True, False])
 @pytest.mark.parametrize("log_scale", [True, False])
-def test_create_from_numpy():
+def test_create_from_numpy(is_normalized: bool, log_scale: bool):
     ...
 
 
 @pytest.mark.parametrize("is_normalized", [True, False])
 @pytest.mark.parametrize("log_scale", [True, False])
-def test_create_from_range():
+def test_create_from_range(is_normalized: bool, log_scale: bool):
     ...
